6aa97b9cd8c404f852c8dfe56d15b6ccefd428c3
Manvan33 <rikostik@gmail.com>
2022-11-18 09:34:05 +0100

    debugging

--- codimdapi.py
+++ codimdapi.py
@@ -1,35 +1,43 @@
 import requests
 
+
 class CodimdAPI:
     def __init__(self, url, email, password):
         self.url = url
         self.email = email
         self.password = password
-    
+
     def login(self):
         self.session = requests.Session()
-        self.session.post(self.url + "login", data={"email": self.email, "password": self.password}, headers={"Referer": self.url})
+        res = self.session.post(self.url + "login", data={
+            "email": self.email, "password": self.password}, headers={"Referer": self.url})
 
     def logged_in_request(self, method, url, **kwargs):
-        try: 
+        print(f"{method} - {url} - args: {kwargs}")
+        try:
             result = self.session.request(method, url, **kwargs)
+            print(result.status_code)
         except AttributeError:
+            print("AttributeError")
             self.login()
             return self.logged_in_request(method, url, **kwargs)
         if result.status_code == 302:
+            print("not logged in")
             self.login()
             return self.logged_in_request(method, url, **kwargs)
         return result
 
     def get_history(self):
-        result = self.logged_in_request("GET", self.url + "history", allow_redirects=False)
+        result = self.logged_in_request(
+            "GET", self.url + "history", allow_redirects=False)
         return result.json()
 
     def refresh_note(self, note_id):
-        result = self.logged_in_request("GET", self.url + "socket.io/?EIO=3&transport=polling&noteId=" + note_id, allow_redirects=False)
+        result = self.logged_in_request(
+            "GET", self.url + "socket.io/?EIO=3&transport=polling&noteId=" + note_id, allow_redirects=False)
         return result.status_code == 200
 
     def delete_note(self, note_id):
-        result = self.logged_in_request("DELETE", self.url + "history/" + note_id, allow_redirects=False)
+        result = self.logged_in_request(
+            "DELETE", self.url + "history/" + note_id, allow_redirects=False)
         return result.status_code == 200
-        
--- main.py
+++ main.py
@@ -2,27 +2,28 @@ from flask import Flask, render_template, request
 from codimdapi import CodimdAPI
 import json
 from os import getenv
-
+from dotenv import load_dotenv
 
 app = Flask(__name__, static_folder="static", template_folder="templates")
 
+load_dotenv()
 CODIMD_FQDN = getenv("CODIMD_FQDN")
 CODIMD_EMAIL = getenv("CODIMD_EMAIL")
 CODIMD_PASSWORD = getenv("CODIMD_PASSWORD")
 
-@app.before_first_request
-def before_first_request():
-    global codimd
-    codimd = CodimdAPI(CODIMD_FQDN, CODIMD_EMAIL, CODIMD_PASSWORD)
+print(CODIMD_EMAIL, CODIMD_FQDN, CODIMD_PASSWORD)
+codimd = CodimdAPI(CODIMD_FQDN, CODIMD_EMAIL, CODIMD_PASSWORD)
 
 
 @app.route('/')
 def index():
     return render_template('index.html', url=CODIMD_FQDN, note_id="")
 
+
 @app.route('/<note_id>')
 def index_with_note(note_id):
     return render_template('index.html', url=CODIMD_FQDN, note_id=note_id+"?"+request.query_string.decode("utf-8"))
+
 
 @app.route('/list')
 def list():
@@ -38,11 +39,15 @@ def list():
             list['unsorted'].append(restructure_entry(entry))
     return list
 
+
 @app.route('/refresh/<note_id>')
 def refresh(note_id):
     return codimd.refresh_note(note_id) and "OK" or (404, "ERROR")
 
-restructure_entry = lambda entry: {"id": entry["id"],"title": entry["text"],"timestamp": entry["time"]}
+
+def restructure_entry(entry): return {
+    "id": entry["id"], "title": entry["text"], "timestamp": entry["time"]}
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
