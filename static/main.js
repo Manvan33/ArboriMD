09ff60771be10c21f49fedeb640d0d5aee046ea8
Manvan33 <rikostik@gmail.com>
2022-11-18 10:32:47 +0100

    state stored to cookies

--- static/main.js
+++ static/main.js
@@ -1,14 +1,73 @@
 
-let state = {
-    "opened": [],
-    "selected": null
+function getFromCookie(key) {
+    let cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i].trim();
+        if (cookie.startsWith(key + "=")) {
+            return cookie.slice(key.length + 1);
+        }
+    }
+    return null;
 }
 
-function loadlist() {
+class State {
+    constructor() {
+        this.state = JSON.parse(getFromCookie("state"));
+        if (this.state == null) {
+            this.state = {
+                "opened": [],
+                "selected": null
+            }
+        }
+        this.storeState();
+    }
+
+    loadState() {
+        this.state = JSON.parse(getFromCookie("state"));
+        return this.state;
+    }
+
+    storeState() {
+        document.cookie = "state=" + JSON.stringify(this.state);
+    }
+
+    getSelected() {
+        return this.loadState().selected;
+    }
+
+    getOpened() {
+        return this.loadState().opened;
+    }
+
+    addOpened(folder) {
+        this.loadState().opened.push(folder);
+        this.storeState();
+    }
+
+    setSelected(id) {
+        this.loadState().selected = id;
+        this.storeState();
+    }
+
+    removeOpened(folder) {
+        let opened = this.loadState().opened;
+        opened.splice(opened.indexOf(folder), 1);
+        this.storeState();
+    }
+}
+
+let state = null;
+
+function pageLoad() {
+    state = new State();
+    state.setSelected(window.location.pathname.slice(1));
+    loadList();
+}
+
+function loadList() {
     fetch("/list")
         .then(response => response.json())
         .then(data => {
-            console.log(data);
             let root = document.querySelector('#arbolist');
             root.innerHTML = "";
             Object.keys(data).forEach(folder => {
@@ -25,13 +84,12 @@ function createFolder(name, data) {
     folder.appendChild(summary);
     summary.addEventListener("click", event => {
         if (folder.open) {
-            state["opened"].pop(name);
+            state.removeOpened(name);
         } else {
-            state["opened"].push(name);
+            state.addOpened(name);
         }
-        console.log(state["opened"]);
     });
-    if (state["opened"].includes(name)) {
+    if (state.getOpened().includes(name)) {
         folder.open = true;
     }
     let ul = document.createElement('ul');
@@ -58,12 +116,12 @@ function note_link(note) {
     let li = document.createElement('li');
     li.innerText = note.title;
     li.setAttribute("id", note.id);
-    if (note.id == state["selected"]) {
+    if (note.id == state.getSelected()) {
         li.classList.add("selected");
     }
     li.addEventListener("click", event => {
         open_note(note.id);
-        state["selected"] = note.id;
+        state.setSelected(note.id);
         li.classList.add("selected");
         document.querySelectorAll("#arbolist li").forEach(li => {
             li.classList.remove("selected");
@@ -75,6 +133,8 @@ function note_link(note) {
 function open_note(id) {
     document.querySelector('#codimd').src = CODIMD_URL + id;
     fetch("/refresh/" + id);
-    state["selected"] = id;
-    loadlist();
+    state.setSelected(id);
+    loadList();
+    // Change url without reloading
+    history.pushState({}, "", "/" + id);
 }
