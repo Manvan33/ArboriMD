6975d4c27c31f17ee31c54f03b57ae8812390d51
Manvan33 <rikostik@gmail.com>
2023-05-16 16:18:27 +0200

    fix: removed secret

--- ws.py
+++ ws.py
@@ -2,12 +2,12 @@
 import requests
 import json
 import websocket
-# -H 'Sec-WebSocket-Key: q0kTpYUGC+e==' -H 'Connection: keep-alive, Upgrade' -H 'Cookie: loginstate=true; userid=7328a5aa-67b2-45c6-81d9-a81ac4813352; indent_type=space; space_units=4; keymap=sublime; logged_out_marketing_header_id=eyJfcmFpbHMiOnsibWVzc2FnZSI6IklqUTBNVFEwWXpNNExXUXhOamt0TkRBM1l5MWhPRGt4TFdKaVpERXlNalExWWpGak1TST0iLCJleHAiOm51bGwsInB1ciI6ImNvb2tpZS5sb2dnZWRfb3V0X21hcmtldGluZ19oZWFkZXJfaWQifX0%3D--27676602079c51a3e16eadffed1078917b1f581e; connect.sid=s%3AWMQJq9fd98TV89iTUwhkbKuOCU6ZywZY.%2BYOvhf1avbuT3q5Odhn1b95NcXmtTiNHNR0CZziOnuM; io=MhNEQ4BhP6lK34G6AALb' -H 'Sec-Fetch-Dest: websocket' -H 'Sec-Fetch-Mode: websocket' -H 'Sec-Fetch-Site: same-origin' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'Upgrade: websocket'
+# -H 'Sec-WebSocket-Key: q0kTpYUGC+EetR7MULXpJw==' -H 'Connection: keep-alive, Upgrade' -H 'Cookie: loginstate=true; userid=7328a5aa-67b2-45c6-81d9-a81ac4813352; indent_type=space; space_units=4; keymap=sublime; logged_out_marketing_header_id=eyJfcmFpbHMiOnsibWVzc2FnZSI6IklqUTBNVFEwWXpNNExXUXhOamt0TkRBM1l5MWhPRGt4TFdKaVpERXlNalExWWpGak1TST0iLCJleHAiOm51bGwsInB1ciI6ImNvb2tpZS5sb2dnZWRfb3V0X21hcmtldGluZ19oZWFkZXJfaWQifX0%3D--27676602079c51a3e16eadffed1078917b1f581e; connect.sid=s%3AWMQJq9fd98TV89iTUwhkbKuOCU6ZywZY.%2BYOvhf1avbuT3q5Odhn1b95NcXmtTiNHNR0CZziOnuM; io=MhNEQ4BhP6lK34G6AALb' -H 'Sec-Fetch-Dest: websocket' -H 'Sec-Fetch-Mode: websocket' -H 'Sec-Fetch-Site: same-origin' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'Upgrade: websocket'
 
 
 
 base_url = "https://notes.rezel.net/"
-note_id = "2-e-bg9lQ"
+note_id = "2-r0QlhBRa6hN6xo-bg9lQ"
 
 # Cookies generated with curl -c cookies.txt -X POST "https://notes.rezel.net/login" --data-raw 'email=user@mail.com&password=yes' -H "Referer: https://notes.rezel.net"
 
